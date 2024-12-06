3c612b503327ad50f79c3e7b7b6a983c6ec2b6e1
ellisrichardj <richard.ellis@apha.gov.uk>
2024-12-06 14:42:13 +0000

    complete functioning of pileup script

--- bin/pileup.sh
+++ bin/pileup.sh
@@ -3,9 +3,11 @@
 set -eo pipefail
 
 sam_in=$1
-#ref=$2
+ref=$2
 
-samtools view $sam_in -q 1 |
-samtools sort - -o sorted.bam -O bam
-#samtools mpileup............, '-L', '1000', '-f', fasta,
-#					 '-Q', str(args.baseq), '-q', str(args.mapq), '-B', out_file_bam_sorted + '.bam'
+samtools view $sam_in -huq 1 -o map.bam #| TODO: Fix piping of these commands
+samtools sort map.bam -o sorted.bam
+samtools mpileup -L 1000 -f $ref -Q 20 -q 1 sorted.bam -o output.pileup
+
+rm map.bam
+rm sorted.bam
