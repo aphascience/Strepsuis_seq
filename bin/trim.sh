67518e1eea6b79599148f80f61f2700836549929
ellisrichardj <richard.ellis@apha.gov.uk>
2025-01-08 15:03:41 +0000

    update trimming parameters

--- bin/trim.sh
+++ bin/trim.sh
@@ -9,4 +9,4 @@ raw2=$3
 adapters=$4
 
 bbduk.sh in=$raw1 in2=$raw2 out="$ID"_trimmed_R1.fastq.gz out2="$ID"_trimmed_R2.fastq.gz ref=$adapters \
-        ktrim=r qtrim=rl trimq=20 ftm=5 minlength=100
+        ktrim=rl k=23 mink=11 hdist=1 tpe tbo qtrim=rl trimq=10 ftm=5
