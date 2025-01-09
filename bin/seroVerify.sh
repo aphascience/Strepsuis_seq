fb693d4d72fc0110916f61798f14d70da576445e
ellisrichardj <richard.ellis@apha.gov.uk>
2025-01-09 22:58:38 +0000

    fix filename

--- bin/seroVerify.sh
+++ bin/seroVerify.sh
@@ -14,6 +14,6 @@ bowtie2 -x $index -1 $read1 -2 $read2 -a --very-sensitive-local --no-unal |
 # create vcf for cps2K
 samtools mpileup -Bf $index.fas sorted.bam -o "$sample"_cps2K.pileup 
 
-base483=$(grep 483 Test_cps2K.pileup | awk '{print $5}' | grep -o -iE 'A|T|C|G|-' | sort | uniq -ic)
+base483=$(grep 483 "$sample"_cps2K.pileup | awk '{print $5}' | grep -o -iE 'A|T|C|G|-' | sort | uniq -ic)
 
 echo "$sample","$base483" > "$sample"_seroVerify.csv
