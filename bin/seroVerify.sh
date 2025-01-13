200bb13f92b999a7ddcecd76c4ecfa4b59c81fc1
ellisrichardj <richard.ellis@apha.gov.uk>
2025-01-13 14:18:19 +0000

    optional running for specific serotypes only

--- bin/seroVerify.sh
+++ bin/seroVerify.sh
@@ -6,14 +6,20 @@ index=$1
 read1=$2
 read2=$3
 sample=$4
+serotype=$5
 
-# map to cps2K
-bowtie2 -x $index -1 $read1 -2 $read2 -a --very-sensitive-local --no-unal |
-    samtools view -b1 - | samtools sort - -o sorted.bam
+if [[ "$serotype" == "2\*" || "$serotype" == "1*" || "$serotype" == "1*?" || "$serotype" == "2" || "$serotype" == "2*" || "$serotype" == "2*?" ]]; then
+    # map to cps2K
+    bowtie2 -x $index -1 $read1 -2 $read2 -a --very-sensitive-local --no-unal |
+        samtools view -b1 - | samtools sort - -o sorted.bam
 
-# create vcf for cps2K
-samtools mpileup -Bf $index.fas sorted.bam -o "$sample"_cps2K.pileup 
+    # create vcf for cps2K
+    samtools mpileup -Bf $index.fas sorted.bam -o "$sample"_cps2K.pileup 
 
-base483=$(grep 483 "$sample"_cps2K.pileup | awk '{print $5}' | grep -o -iE 'A|T|C|G|-' | sort | uniq -ic)
+    base483=$(grep 483 "$sample"_cps2K.pileup | awk '{print $5}' | grep -o -iE 'A|T|C|G|-' | sort | uniq -ic)
 
-echo "$sample","$base483" > "$sample"_seroVerify.csv
+    echo "$sample","$base483" > "$sample"_seroVerify.csv
+
+else
+    echo "Verification not required"
+fi
