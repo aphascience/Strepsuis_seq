3278f10c181a6bc74050e5d486864932cf0710aa
ellisrichardj <richard.ellis@apha.gov.uk>
2025-01-13 23:29:49 +0000

    extract basecall and evidence from vcf

--- bin/seroVerify.sh
+++ bin/seroVerify.sh
@@ -16,11 +16,18 @@ if [[ "$serotype" == "1" || "$serotype" == "1*" || "$serotype" == "1*?" || "$ser
     # create vcf for cps2K
     ~/biotools/samtools-0.1.18/samtools mpileup -DBuf $index.fas sorted.bam | ~/biotools/samtools-0.1.18/bcftools/bcftools view -c - > "$sample"_cps2K.vcf 
 
-    verifyevidence=$(grep 483 "$sample"_cps2K.vcf | awk '{print $5}' | grep -o -iE 'A|T|C|G|-' | sort | uniq -ic)
-    base483=($verifyevidence)
+    # extract basecall and  evidence from vcf
+    ref483=$(grep -w 483 "$sample"_cps2K.vcf | awk '{print $4}')
+    alt483=$(grep -w 483 "$sample"_cps2K.vcf | awk '{print $5}')
+    evidence=$(grep -w 483 "$sample"_cps2K.vcf | awk '{print $8}' | awk -F ';' '{print $5}' | awk -F '=' '{print $2}')
+    if $alt483='.'; then
+        base483=$ref483
+    else
+        base483=$alt483
+    fi
 
-    echo "Sample,Pos483,Count" > "$sample"_seroVerify.csv
-    echo "$sample","${base483[1]}",${base483[0]} >> "$sample"_seroVerify.csv
+    echo "Sample,Pos483,RefF,RefR,AltF,AltR" > "$sample"_seroVerify.csv
+    echo "$sample","$base483","$evidence" >> "$sample"_seroVerify.csv
 
 else
     echo "Verification not required"
