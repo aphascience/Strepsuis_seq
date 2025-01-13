88ada9480678ac1c863d4a908992e01c3fb0b6ad
ellisrichardj <richard.ellis@apha.gov.uk>
2025-01-13 22:38:14 +0000

    Create vcf using samtools v0.1.18

--- bin/seroVerify.sh
+++ bin/seroVerify.sh
@@ -8,15 +8,15 @@ read2=$3
 sample=$4
 serotype=$5
 
-if [[ "$serotype" == "2\*" || "$serotype" == "1*" || "$serotype" == "1*?" || "$serotype" == "2" || "$serotype" == "2*" || "$serotype" == "2*?" ]]; then
+if [[ "$serotype" == "1" || "$serotype" == "1*" || "$serotype" == "1*?" || "$serotype" == "2" || "$serotype" == "2*" || "$serotype" == "2*?" ]]; then
     # map to cps2K
     bowtie2 -x $index -1 $read1 -2 $read2 -a --very-sensitive-local --no-unal |
-        samtools view -b1 - | samtools sort - -o sorted.bam
+        ~/biotools/samtools-0.1.18/samtools view -S1 - | ~/biotools/samtools-0.1.18/samtools sort - sorted
 
     # create vcf for cps2K
-    samtools mpileup -Bf $index.fas sorted.bam -o "$sample"_cps2K.pileup 
+    ~/biotools/samtools-0.1.18/samtools mpileup -DBuf $index.fas sorted.bam | ~/biotools/samtools-0.1.18/bcftools/bcftools view -c - > "$sample"_cps2K.vcf 
 
-    verifyevidence=$(grep 483 "$sample"_cps2K.pileup | awk '{print $5}' | grep -o -iE 'A|T|C|G|-' | sort | uniq -ic)
+    verifyevidence=$(grep 483 "$sample"_cps2K.vcf | awk '{print $5}' | grep -o -iE 'A|T|C|G|-' | sort | uniq -ic)
     base483=($verifyevidence)
 
     echo "Sample,Pos483,Count" > "$sample"_seroVerify.csv
